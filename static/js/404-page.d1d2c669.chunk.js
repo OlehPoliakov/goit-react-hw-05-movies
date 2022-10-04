2d16edf08dc678b671c8704a7b201e9ec3393394
OlehPoliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-10-04 16:26:55 +0000

    Deploying to gh-pages from @ OlehPoliakov/goit-react-hw-05-movies@82a766e4666697ba92b357b72fcc8ec8d99aa0d6 🚀

---
+++ static/js/404-page.d1d2c669.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[968],{333:function(e,r,t){t.r(r);var a=t(184);r.default=function(){return(0,a.jsxs)("h1",{style:{padding:20,textAlign:"center"},children:["Error 404: Page Not Found"," ",(0,a.jsx)("span",{role:"img","aria-label":"sheep",children:"\ud83d\ude2e"})]})}}}]);
+//# sourceMappingURL=404-page.d1d2c669.chunk.js.map
